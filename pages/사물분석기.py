9ca6dd609e55240f7b156cb7cc7672a63ab046a4
starrylife <forinnocen@naver.com>
2024-02-22 09:37:47 +0900

    0222_11

--- pages/사물분석기.py
+++ pages/사물분석기.py
@@ -48,7 +48,7 @@ if password == correct_password:
                     {
                         "role": "user",
                         "content": [
-                            {"type": "text", "text": "이건 어떤 그림입니까?"},
+                            {"type": "text", "text": "Please analyze the meal and write down the nutrients that are lacking and the recommended foods."},
                             {"type": "image_url", "image_url": {"url": file_url}},
                         ],
                     }
